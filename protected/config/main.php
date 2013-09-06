7c7440c9edd42cfeabef6512561688c66426cab3
TCHUIMAMO KAMGA Pecos <tchuimamokp@gmail.com>
2013-09-06 19:20:39 +0100

    mise à jour code par pecos

--- protected/config/main.php
+++ protected/config/main.php
@@ -8,6 +8,7 @@
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Gestion Scolaire',
+      //  'theme'=>'twitter_fluid',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
